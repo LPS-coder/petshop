a72f0f82c5e0eb78140f75bcff2706c4f951d2b1
LPS-coder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-08-26 08:30:29 -0300

    FEAT - Index.php

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>PetShop AUmigos - Início</title>
+</head>
+<body>
+    <h1>PetShop AUmigos </h1>
+    <p>Bem-vindo ao sistema de gerenciamento! Selecione uma opção para navegar:</p>
+
+    <ul>
+        <li>
+            <a href="clientes/listar_clientes.php">Gerenciar Clientes</a>
+        </li>
+        <li>
+            <a href="animais/listar_animais.php">Gerenciar Animais</a>
+        </li>
+    </ul>
+</body>
+</html>
