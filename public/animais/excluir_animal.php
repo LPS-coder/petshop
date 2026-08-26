1b2421f8c72bd4dbc6478081905d66852252e5c7
LPS-coder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-08-26 08:16:56 -0300

    FEAT - Excluir animal

--- public/animais/excluir_animal.php
+++ public/animais/excluir_animal.php
@@ -1,17 +1,17 @@
 <?php
 require_once '../../infra/conexao.php';
 
-$cliente_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+$animal_id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-if ($cliente_id) {
+if ($animal_id) {
     try {
-        $sql = "DELETE FROM clientes WHERE id = :id";
+        $sql = "DELETE FROM animais WHERE id = :id";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([':id' => $cliente_id]);
+        $stmt->execute([':id' => $animal_id]);
     } catch (PDOException $e) {
-        die("Erro ao excluir cliente: " . $e->getMessage());
+        die("Erro ao excluir animal: " . $e->getMessage());
     }
 }
 
-header('Location: listar_clientes.php?sucesso=1');
+header('Location: listar_animais.php?sucesso=1');
 exit;
